ef360f524820230709f4cd2010bb0abd8c6f724d
JOTHI <jothirajkumarerode@gmail.com>
2026-03-22 22:12:57 +0530

    Refactor palindrome checker into UseCase2 class

--- app/src/PalindroneCheckerApp.java
+++ app/src/PalindroneCheckerApp.java
@@ -1,6 +1,24 @@
-public class PalindroneCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to palindrome Checker App Management System");
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String text = "madam";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed = reversed + text.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (text.equals(reversed)) {
+            System.out.println("The string \"" + text + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + text + "\" is NOT a Palindrome.");
+        }
     }
 }
 
